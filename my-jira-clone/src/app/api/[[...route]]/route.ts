25345ca969d5757c1be5e161b2b1cf80aa436623
KT1 <braian_bg@hotmail.es>
2024-10-28 21:53:42 +0100

    added Hono

--- my-jira-clone/src/app/api/[[...route]]/route.ts
+++ my-jira-clone/src/app/api/[[...route]]/route.ts
@@ -3,13 +3,4 @@ import { handle } from "hono/vercel"
 
 const app = new Hono().basePath("/api")
 
-app.get("/hello", (c) => {
-  return c.json({ hello: "world" })
-})
-
-app.get("project/:projectId", (c) => {
-  const { projectId } = c.req.param()
-  return c.json({ project: projectId })
-})
-
 export const GET = handle(app)
